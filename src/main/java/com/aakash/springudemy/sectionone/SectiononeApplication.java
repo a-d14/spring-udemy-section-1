2c33398159c3399bcf430f6c259eadb5393b4add
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2023-07-02 16:53:02 +0530

    section-1-spring-basics

---
+++ src/main/java/com/aakash/springudemy/sectionone/SectiononeApplication.java
@@ -0,0 +1,13 @@
+package com.aakash.springudemy.sectionone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectiononeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectiononeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aakash/springudemy/sectionone/SectiononeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aakash.springudemy.sectionone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectiononeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
